fe388f03a7347c1a35c88e3ccb27cde37138233e
hirose504 <hirose504@yahoo.co.jp>
2020-01-21 00:24:50 +0900

    add nuxt app

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const express = require('express')
+const { Nuxt } = require('nuxt')
+const app = express()
+
+// Import and Set Nuxt.js options
+const config = {
+  dev: false,
+  buildDir: 'nuxt'
+}
+
+// Init Nuxt.js
+const nuxt = new Nuxt(config)
+
+app.use(async (req, res) => {
+  await nuxt.ready()
+  nuxt.render(req, res)
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.nuxtApp = functions.https.onRequest(app)
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!')
+})
